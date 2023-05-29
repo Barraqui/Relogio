5159fe8eb8fddb7b8bcc96dd7081b5d7f0fa6235
Gabriel Martins Barraqui <gabrielmartinsbarraqui@gmail.com>
2023-05-29 10:45:03 -0300

    mudança no script

--- script.js
+++ script.js
@@ -1,4 +1,50 @@
-const horas = document.querySelector('horas');
-const min = document.querySelector('minutos');
-const seg = document.querySelector('segundos');
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
 
+const relogio = setInterval(function tempo() {
+    let dateToday = new Date();
+    let hours = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+
+    if(hours < 10) {
+        hours = '0' + hours;
+    }
+
+    if(min < 10) {
+        min = '0' + min;
+    }
+
+    if(sec < 10) {
+        sec = '0' + sec;
+    }
+    
+    horas.textContent = hours;
+    minutos.textContent = min;
+    segundos.textContent = sec;
+})
+clearInterval(relogio);
+
+const otherHours = setInterval(function tempo() {
+    let dateToday = new Date();
+    let hours = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+
+    if(hours < 10) {
+        hours = '0' + hours;
+    }
+
+    if(min < 10) {
+        min = '0' + min;
+    }
+
+    if(sec < 10) {
+        sec = '0' + sec;
+    }
+    
+    horas.textContent = hours;
+    minutos.textContent = min;
+    segundos.textContent = sec;
+})
